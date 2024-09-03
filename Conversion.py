967f158338b7a77f6a584993beb77a0367d26f3f
Nico <nvithana@mywvm.wvm.edu>
2024-09-03 12:12:32 -0700

    Update Conversion.py
    Finished part 3 & changed some variable names

--- Conversion.py
+++ Conversion.py
@@ -1,5 +1,5 @@
 print("Week 2 Lab project\nOption 1 - KM to Miles\nOption 2 - Celsius to Fahrenheit\nOption 3 - Binary to Decimal")
-choose_tool = int(input("Choose a conversion tool: "))
+chooseTool = int(input("Choose a conversion tool: "))
 
 def convert_distance(number):
     return input / 1.609344 # 1 mile = 1.609344 km so we divide to get result
@@ -7,9 +7,15 @@ def convert_distance(number):
 def convert_temp(number):
     return (input * 1.8) + 32 # fahrenheit = celsius x 1.8 + 32
 
-if choose_tool == 1:
+def convert_binary(number):
+    return int(str(number), 2) # returns base 2 number as a decimal number
+
+if chooseTool == 1:
     input = float(input("Input Distance in Kilometers: "))
     print(f"Distance in Miles: {convert_distance(number = input)} mile(s)")
-if choose_tool == 2:
+if chooseTool == 2:
     input = float(input("Input Temperature in Celsius: "))
     print(f"Temperature in Fahrenheit: {convert_temp(number = input)} °F")
+if chooseTool == 3:
+    input = input("Input Number in Binary: ")
+    print(f"Number in Decimal: {convert_binary(number = input)}")
